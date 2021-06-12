a5ae0983bdde86a25df77e6c069a6658267a13e8
Nihal Kumar Das <nihalrahulindia@gmail.com>
2021-06-12 12:02:37 +0530

    Added Files

---
+++ BoxingAndUnboxingOperation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BoxingAndUnboxingOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boxing and Unboxing Operations!");
+
+            //Declare a Variable
+            int x = 20;
+            Console.WriteLine($"x = {x}");
+
+            //Perform the Boxing Operation
+            object obj = x; //BOXING
+            Console.WriteLine($"Object = {obj}");
+
+            //Perform the Unboxing Operation
+            int y = (int)obj; //UNBOXING
+            Console.WriteLine($"y = {y}");
+        }
+    }
+}
